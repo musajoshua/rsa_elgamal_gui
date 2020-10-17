3c4fc85c594bbb205e62f623cdfdb67ebe7ae211
Musa Joshua <gidijosh@gmail.com>
2020-10-17 17:13:46 +0100

    clean up

--- web/rsa/decrypt.js
+++ web/rsa/decrypt.js
@@ -1,6 +1,32 @@
+let fileData;
+let extn;
+
+$(document).ready(function () {
+	$("#encrypted_file").on("change", async function () {
+		var imgPath = $(this)[0].value;
+		extn = imgPath.substring(imgPath.lastIndexOf(".") + 1).toLowerCase();
+		var file_holder = $("#textarea1");
+		file_holder.empty();
+		if (typeof FileReader != "undefined") {
+			fileData = await readFile($(this)[0].files[0]);
+
+			document.getElementById("encrypted_file_view").value = fileData;
+		} else {
+			alert("This Application does not support FileReader.");
+		}
+	});
+});
+
 submit = async () => {
-	// get public keys
-	// let public_keys = document.getElementById("public_key").value;
+	// get private keys
+	// let private_keys = document.getElementById("private_key").value;
+	// if (!private_keys) {
+	// 	alert("Please Enter Your Public Key");
+	// 	return;
+	// }
+	// const [n, e] = private_keys.split(",");
+
+	// private_keys = [parseInt(n), parseInt(e)];
 	let private_keys = [198131, 120013];
 	// get file for encryption
 	let file = document.getElementById("encrypted_file").files[0];
@@ -9,55 +35,71 @@ submit = async () => {
 		return;
 	}
 
-	// console.log(file);
+	eel.rsa_decrypt(
+		private_keys,
+		fileData
+	)((data) => {
+		let plain_string = data[0];
+		let time_taken = data[1];
 
-	let reader = new FileReader();
-	reader.onload = async () => {
-		let dataURL = await reader.result;
-		document.getElementById("textarea1").value = dataURL;
-		eel.rsa_decrypt(
-			private_keys,
-			dataURL
-		)((data) => {
-			let cipher_string = data[0];
-			// console.log(cipher_string);
-			let time_taken = data[1];
+		document.getElementById("time_taken_to_decrypt").value = time_taken;
 
-			console.log(cipher_string);
+		var file_holder = $("#file-holder");
 
-			var blob = new Blob([cipher_string], {
-				// type: "text/plain;charset=utf-8",
-				type: "image/png",
+		if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+			$("<img />", {
+				src: plain_string,
+				width: "100%",
+				height: "100%",
+			}).appendTo(file_holder);
+			file_holder.show();
+		} else if (extn == "mp3" || extn == "flac") {
+			const source = $("<source />", {
+				src: plain_string,
 			});
-			saveAs(blob, "enc.png");
-
-			document.getElementById("time_taken_to_decrypt").value = time_taken;
-			// document.getElementById("encrypted_file").value = cipher_string;
-		});
-	};
-	reader.readAsText(file);
-	// let dd = await reader.result;
-	// console.log(dd);
-
-	// let fileURL = URL.createObjectURL(file);
-	// console.log(file);
-
-	// eel.rsa_encrypt(
-	// 	public_keys,
-	// 	file
-	// )((data) => {
-	// 	// alert(data);
-	// 	console.log(data);
-	// });
+			const audio = $("<audio controls />", {
+				// width: "100%",
+				height: "300px",
+			});
+			source.appendTo(audio);
+			audio.appendTo(file_holder);
+			file_holder.show();
+		} else if (extn == "mp4" || extn == "mov") {
+			const source = $("<source />", {
+				src: plain_string,
+			});
+			const video = $("<video controls />", {
+				// width: "100%",
+				height: "300px",
+			});
+			source.appendTo(video);
+			video.appendTo(file_holder);
+			file_holder.show();
+		} else {
+			alert("Pls select only images, video and audio");
+		}
+	});
 };
 
 download = () => {
-	let cipher_string = document.getElementById("encrypted_file").value;
-	if (!cipher_string) {
+	let dataURI = document.getElementById("file-holder").value;
+	if (!dataURI) {
 		alert("No file to download");
 		return;
 	}
 
-	var blob = new Blob([cipher_string], { type: "text/plain;charset=utf-8" });
-	saveAs(blob, "enc.enc");
+	var byteString = atob(dataURI.split(",")[1]);
+
+	// separate out the mime component
+	var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+	// write the bytes of the string to an ArrayBuffer
+	var ab = new ArrayBuffer(byteString.length);
+	var ia = new Uint8Array(ab);
+	for (var i = 0; i < byteString.length; i++) {
+		ia[i] = byteString.charCodeAt(i);
+	}
+	let blob = new Blob([ab], { type: mimeString });
+
+	saveAs(blob, `dec.${mimeString.split("/")[1]}`);
 };
