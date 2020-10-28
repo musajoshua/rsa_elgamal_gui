d8719cb080a997b0a56ddabb804212a9d198edc5
Musa Joshua <gidijosh@gmail.com>
2020-10-28 15:46:21 +0100

    add docs to home.py

--- home.py
+++ home.py
@@ -1,3 +1,12 @@
+# -----------------------------------------------------------
+# Main Python File
+#
+# (C) 2020 Musa Joshua Gideon-Bashir, Ilorin, Nigeria
+# Released under MIT Public License
+# email gidijosh@gmail.com
+# -----------------------------------------------------------
+
+
 import io
 from mod.modules.RSA import RSA
 from mod.modules.ELGamal import ELGamal
@@ -6,17 +15,34 @@ from base64 import b64encode
 import eel
 import binascii
 
+# initialize eel for root directory ./web
 eel.init('web')
 
 # RSA KEY GENERATION
 @eel.expose
 def rsa_key_gen():
+    """Generates RSA keys and sends it to the eel GUI
+
+    Returns:
+        int: public key
+        int: private key
+    """
     pu_key, pr_key = RSA.generate_keys()
     return (pu_key, pr_key)
 
 # RSA ENCRYPTION
 @eel.expose
 def rsa_encrypt(public_key, message):
+    """Encrypt message with rsa
+
+    Args:
+        public_key: array of n and e
+        message: string of message
+
+    Returns:
+        str: string of encrypted message
+        timestamp: time taken to encrypt
+    """
     mime = message.split(",")[0]
     mess_data = message.split(",")[1]
     n = public_key[0]
@@ -29,6 +55,16 @@ def rsa_encrypt(public_key, message):
 # RSA DECRYPTION
 @eel.expose
 def rsa_decrypt(private_key, cipher):
+    """Decrypt cipher with rsa
+
+    Args:
+        private_key: array of n and d
+        cipher: string of message
+
+    Returns:
+        str: string of decrypted message
+        timestamp: time taken to encrypt
+    """
     mime = cipher.split(",")[0]
     cipher_data = cipher.split(",")[1]
     n = private_key[0]
@@ -40,12 +76,29 @@ def rsa_decrypt(private_key, cipher):
 # ELGamal KEY GENERATION
 @eel.expose
 def elgamal_key_gen():
+    """Generates ELGamal keys and sends it to the eel GUI
+
+    Returns:
+        int: public key
+        int: private key
+    """
     pu_key, pr_key = ELGamal.generate_keys()
     return (pu_key, pr_key)
 
 # ELGamal ENCRYPTION
 @eel.expose
 def elgamal_encrypt(public_key, message):
+    """Encrypt message with ELGamal
+
+    Args:
+        public_key: array of p, g and y
+        message: string of message
+
+    Returns:
+        str: string of encrypted message 1
+        str: string of encrypted message 2
+        timestamp: time taken to encrypt
+    """
     mime = message.split(",")[0]
     mess_data = message.split(",")[1]
     p = public_key[0]
@@ -60,6 +113,17 @@ def elgamal_encrypt(public_key, message):
 # ELGamal DECRYPTION
 @eel.expose
 def elgamal_decrypt(private_key, cipher_string1, cipher_string2):
+    """Decrypt message with ELGamal
+
+    Args:
+        private_key: array of p and x
+        cipher_string1: string of encrypted message 1
+        cipher_string2: string of encrypted message 2
+
+    Returns:
+        str: string of decrypted message
+        timestamp: time taken to encrypt
+    """
     mime = cipher_string2.split(",")[0]
     cipher_data1 = cipher_string1.split(",")[1]
     cipher_data2 = cipher_string2.split(",")[1]
@@ -71,4 +135,5 @@ def elgamal_decrypt(private_key, cipher_string1, cipher_string2):
     return (mime + "," + plain, time_taken_decrypt)
 
 
+# start eel with index as index.html
 eel.start('index.html', size=(1050, 650))
