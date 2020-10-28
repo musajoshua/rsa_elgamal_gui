4a224802bde8ee32eb4f0e4b39f90dd7488e998e
Musa Joshua <gidijosh@gmail.com>
2020-10-28 13:14:37 +0100

    add docs to encoding file

--- mod/modules/encoding.py
+++ mod/modules/encoding.py
@@ -1,7 +1,24 @@
+# -----------------------------------------------------------
+# Encoding and Decoding Functions that Performs the Encoding and Decoding of a string to ascii
+#
+# (C) 2020 Musa Joshua Gideon-Bashir, Abuja, Nigeria
+# Released under MIT Public License
+# email gidijosh@gmail.com
+# -----------------------------------------------------------
+
+
 import base64
 
 
 def encode(num_string):
+    """Endocdes a string of numbers
+
+    Args:
+        num_string: String of Numbers to be encoded
+
+    Returns:
+        str: an encoded string of alphabets
+    """
     encoded_number_string = num_string.encode('ascii')
     base64_encoded_number_string = base64.b64encode(encoded_number_string)
     base64_string = str(base64_encoded_number_string, 'ascii')
@@ -9,6 +26,14 @@ def encode(num_string):
 
 
 def decode(cipher_string):
+    """Endocdes a string of numbers
+
+    Args:
+        cipher_string: an encoded string of alphabets
+
+    Returns:
+        str: an decoded string of numbers
+    """
     decoded_number_string = cipher_string.encode('ascii')
     base64_decoded_number_string = base64.b64decode(decoded_number_string)
     base64_string = str(base64_decoded_number_string, 'ascii')
